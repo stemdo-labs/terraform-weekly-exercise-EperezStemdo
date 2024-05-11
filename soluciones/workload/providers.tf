9d903a4f05eebeeb639a904aae473a33044f284c
EperezStemdo <166404865+EperezStemdo@users.noreply.github.com>
2024-05-11 19:01:15 +0200

    Update providers.tf

--- soluciones/workload/providers.tf
+++ soluciones/workload/providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {
-      resource_group_name  = "rg1eperez-lab01"
-      storage_account_name = "sta1eperez" 
-      container_name       = "tfstateeperez"
-      key                  = "terraform.tfstate"
-  }
+ # backend "azurerm" {
+ #     resource_group_name  = "rg1eperez-lab01"
+ #     storage_account_name = "sta1eperez" 
+ #     container_name       = "tfstateeperez"
+ #     key                  = "terraform.tfstate"
+ # }
 
 }
 
